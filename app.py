b0f9c936fde62a833966a203641bfdc88c685f8a
aaronsohal87-ui <aaronsohal87@gmail.com>
2026-08-04 14:51:34 +0100

    Fix size category logic in get_size function
    Refactor get_size function to handle size categories more clearly.

--- app.py
+++ app.py
@@ -40,13 +40,17 @@ if uploaded_file is not None: # prevents app from crashing without an uploaded f
         df["Package Height"] = df["Package Height"].str.replace(" cm","").astype(float) #Takes value in csv from Height measurement into number using float
 
         df["Longest Side"] = df[["Package Length", "Package Width", "Package Height"]].max(axis=1) # .max(axis=1) looks at all 3 values and picks the largest value
+
+        def get_size(longest):
+            if longest <= 35:
                 return "Small"
             elif longest <= 45:
                 return "Medium"
-            elif longest <= 61: 
+            elif longest <= 61:
                 return "Small Oversize"
-            elif longest <= 75: 
-                return "Large Oversize" 
+            else:
+                return "Large Oversize"
+             
         
         df["Size Category"] = df["Longest Side"].apply(get_size) #records longest side of package and saves it in size category with get_size function applied to it
         st.write(" Lost Parcel Size Breakdown")
